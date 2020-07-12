83b101d7529f6197e16408904c866c92b6639bdd
isratnishi <isratjahan27cse@gmail.com>
2020-07-12 12:04:47 +0600

    search done

--- app/src/main/java/com/opus_bd/myapplication/Activity/Fargment/ChatFragment.java
+++ app/src/main/java/com/opus_bd/myapplication/Activity/Fargment/ChatFragment.java
@@ -1,9 +1,13 @@
 package com.opus_bd.myapplication.Activity.Fargment;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -22,6 +26,7 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -29,7 +34,10 @@ import retrofit2.Response;
 public class ChatFragment extends Fragment {
     @BindView(R.id.rvUserList)
     RecyclerView rvUserList;
-
+    @BindView(R.id.et_search)
+    EditText et_search;
+    @BindView(R.id.bt_clear)
+    ImageView bt_clear;
     MemberListAdapter memberListAdapter;
     ArrayList<UserListModel> UserListModel = new ArrayList<>();
 
@@ -44,16 +52,35 @@ public class ChatFragment extends Fragment {
         return v;
     }
 
+    TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void onTextChanged(CharSequence c, int i, int i1, int i2) {
+            if (c.toString().trim().length() == 0) {
+                bt_clear.setVisibility(View.GONE);
+            } else {
+                getSearchData(c.toString());
+                bt_clear.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence c, int i, int i1, int i2) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+        }
+    };
 
     public void intRecyclerView() {
         memberListAdapter = new MemberListAdapter(UserListModel, getContext());
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
         rvUserList.setLayoutManager(layoutManager);
         rvUserList.setAdapter(memberListAdapter);
+        et_search.addTextChangedListener(textWatcher);
     }
 
-
-    private void getAllUser() {
+    public void getAllUser() {
         RetrofitService retrofitService = RetrofitClientInstance.getRetrofitInstance().create(RetrofitService.class);
         int id = Integer.parseInt(SharedPrefManager.getInstance(getContext()).getUserID());
 
@@ -77,5 +104,30 @@ public class ChatFragment extends Fragment {
             }
         });
 
+    }
+
+    @OnClick(R.id.bt_clear)
+    public void getData() {
+        et_search.setText(" ");
+        intRecyclerView();
+        getAllUser();
+    }
+
+    private void getSearchData(String text) {
+        ArrayList<UserListModel> tempItems = new ArrayList<>();
+        memberListAdapter = new MemberListAdapter(tempItems, getContext());
+        rvUserList.setAdapter(memberListAdapter);
+
+        for (int i = 0; i < UserListModel.size(); i++) {
+            UserListModel tempModel = UserListModel.get(i);
+            if (tempModel.getUserName().toLowerCase().contains(text.toLowerCase()) || tempModel.getEmpName().toLowerCase().contains(text.toLowerCase())) {
+                tempItems.add(tempModel);
+            }
+        }
+
+        if (tempItems.isEmpty()) {
+            memberListAdapter = new MemberListAdapter(UserListModel, getContext());
+        }
+        memberListAdapter.notifyDataSetChanged();
     }
 }
